ed66d45af64dcc3a325851d460aee61df58b8f1d
Ram Vashakan <vramvasu99@gmail.com>
2020-05-14 10:54:47 +0530

    UI hotel page,form for adding items

--- src/pages/hotel/hotel.ts
+++ src/pages/hotel/hotel.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the HotelPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -20,6 +15,11 @@ export class HotelPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HotelPage');
+  }
+
+  update(){
+
+    
   }
 
 }

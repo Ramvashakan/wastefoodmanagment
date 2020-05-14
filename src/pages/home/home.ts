9e36d9a58715124711057bebe9e9c5e629863175
Ram Vashakan <vramvasu99@gmail.com>
2020-05-14 08:01:26 +0530

    Login page UI,SignUp page UI

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { SignupPage } from './../signup/signup';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  signup(){
+
+    this.navCtrl.push(SignupPage)
+
+  }
+
+}

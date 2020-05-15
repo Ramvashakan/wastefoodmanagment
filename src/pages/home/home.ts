29d824b68214aaa2c3c9db8e5c16699d1a0e4381
Ram Vashakan <vramvasu99@gmail.com>
2020-05-15 21:32:32 +0530

    login page backend email login added all alert messages and loding controller

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,28 @@
 import { HotelPage } from './../hotel/hotel';
 import { SignupPage } from './../signup/signup';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  email:any;
+  password:any;
 
-  }
+  constructor(public navCtrl: NavController,
+    
+    public AF:AngularFireAuth,
+    public loading:LoadingController,
+    public AlrtCtrl:AlertController
+
+    ) { }
 
   signup(){
 
@@ -19,8 +30,95 @@ export class HomePage {
 
   }
 
-  login(){
-    this.navCtrl.push(HotelPage);
-  }
+  /*.........................LOGIN PAGE....................................... */
 
+  login(){
+
+    if(this.email == null){
+
+      let alert = this.AlrtCtrl.create({
+
+        title:'Error',
+        message: 'Enter an valid Email id',
+        buttons: [{
+
+          text:'OK',
+          handler: ()=>{
+
+            this.email = null;
+            this.password = null;
+
+          }
+          
+        }]
+
+      });
+
+      alert.present();
+
+    }
+
+    else if(this.password == null){
+
+      let alert = this.AlrtCtrl.create({
+
+        title:'Error',
+        message: 'Enter an valid password',
+        buttons: [{
+
+          text:'OK',
+          handler: ()=>{
+
+            this.email = null;
+            this.password = null;
+
+          }
+          
+        }]
+
+      });
+
+      alert.present();
+
+    }
+
+    else{
+
+    this.AF.auth.signInWithEmailAndPassword(this.email,this.password).then(result =>{
+
+      let load = this.loading.create({
+
+        content:'Signing In..',
+        dismissOnPageChange:true,
+        spinner:'ios'
+      });
+
+      load.present();
+
+    }).catch(err => {
+
+        let alert = this.AlrtCtrl.create({
+
+          title:'Error',
+          message: err,
+          buttons: [{
+
+            text:'OK',
+            handler: ()=>{
+
+              this.email = null;
+              this.password = null;
+
+            }
+            
+          }]
+
+        });
+
+        alert.present();
+
+    });
+  }
 }
+}
+

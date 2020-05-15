c9b1bc8caa0bcf38a3b9df2c6a4ebcf4bf0d7664
Ram Vashakan <vramvasu99@gmail.com>
2020-05-15 11:56:19 +0530

    sign up page backend for hotel(email authentication)

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,6 @@
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,AlertController,LoadingController } from 'ionic-angular';
 
 import{ AngularFireAuth } from 'angularfire2/auth';
 
@@ -19,8 +19,9 @@ export class SignupPage {
   
   constructor(public navCtrl: NavController, public navParams: NavParams,
     
-    public AF:AngularFireAuth
-    
+    public AF:AngularFireAuth,
+    public AlrtCtrl:AlertController,
+    public Loading:LoadingController
     ) {
   }
 
@@ -33,14 +34,95 @@ export class SignupPage {
     this.navCtrl.push(HomePage)
   }
 
-  hotel(){
 
-    
+  create_hotel(){
+
+    if(this.email == null ){
+
+      let alert = this.AlrtCtrl.create({
+
+        title: 'Error',
+        message:'Enter the valid email',
+        buttons:['OK']
+      });
+
+      alert.present();  
+    }
+
+    else if(this.password == null){
+
+      let alert = this.AlrtCtrl.create({
+
+        title: 'Error',
+        message:'Enter the valid Password',
+        buttons:['OK']
+      });
+
+      alert.present();  
+    }
+
+    else if( this.hotel_name == null){
+      let alert = this.AlrtCtrl.create({
+
+        title: 'Error',
+        message:'Enter the valid Hotel Name',
+        buttons:['OK']
+      });
+
+      alert.present();  
+    }
+
+    else if(this.mobile_phn ==null){
+
+      let alert = this.AlrtCtrl.create({
+
+        title: 'Error',
+        message:'Enter the valid Mobile number',
+        buttons:['OK']
+      });
+
+      alert.present();  
+    }
+
+    else{
+
+    this.AF.auth.createUserWithEmailAndPassword(this.email,this.password).then(result =>{
+
+      console.log(result);
+
+     let alert = this.AlrtCtrl.create({
+
+        title: 'Success',
+        message:'Account created Successfull',
+        buttons:[{
+          text:'OK',
+          handler: () =>{
+
+            let load = this.Loading.create({
+              spinner:'ios',
+              dismissOnPageChange:true,
+              content:'loading'
+            });
+            load.present();
+            this.navCtrl.push(HomePage);
+            
+          }
+        }]
+      });
+
+      alert.present();
+    }).catch(err =>{
+
+      let alert = this.AlrtCtrl.create({
+
+        title: 'Error',
+        message:err,
+        buttons:['OK']
+      });
+
+      alert.present();  
+    });
   }
-
-  create(){
-
-    
 
   }
 

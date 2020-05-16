71d822fe22fb688c8606a7118663099d5da817cc
Ram Vashakan <vramvasu99@gmail.com>
2020-05-16 18:02:43 +0530

    css work in hotelDetalilsPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = SignupPage;
+  rootPage:any = HoteldetailsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {

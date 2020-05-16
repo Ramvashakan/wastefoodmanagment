77b83448c5b91adac284ee0f88a583385826533e
Ram Vashakan <vramvasu99@gmail.com>
2020-05-16 18:14:02 +0530

    css work in asramamDetailPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HoteldetailsPage;
+  rootPage:any = AsramamdetailsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
